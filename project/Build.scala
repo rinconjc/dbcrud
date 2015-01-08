688edbd900b3a8d06164b72fdb4c3e0f2cb8cb5a
Julio Rincon <julio.rincon@transport.nsw.gov.au>
2015-01-08 16:13:35 +1100

    reformatting

--- project/Build.scala
+++ project/Build.scala
@@ -2,12 +2,12 @@ import sbt._
 import Keys._
 
 object DbCrudBuild extends Build {
-  lazy val root = Project(id = "dbcrud",
-    base = file(".")) aggregate(core, rest)
+  lazy val root = Project(id = "dbcrud", base = file(".")) aggregate(core, rest)
 
-  lazy val core = Project(id = "dbcrud-core",
-    base = file("core"))
+  lazy val core = Project(id = "dbcrud-core", base = file("core"))
 
-  lazy val rest = Project(id = "dbcrud-rest",
-    base = file("rest"))
+  lazy val rest = Project(id = "dbcrud-rest", base = file("rest"))
+
+  scalaVersion:="2.11.2"
+
 }
